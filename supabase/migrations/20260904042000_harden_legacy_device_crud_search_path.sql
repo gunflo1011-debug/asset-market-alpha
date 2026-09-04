d3c666bf64211ebebc977a3e00326e2da30cf443
gunflo1011-debug <gunflo1011@gmail.com>
2026-09-04 04:11:11 +0200

    Harden legacy device CRUD search paths

---
+++ supabase/migrations/20260904042000_harden_legacy_device_crud_search_path.sql
@@ -0,0 +1,2 @@
+alter function public.update_private_device(uuid,text,text,text,boolean,boolean,smallint,boolean,boolean) set search_path = '';
+alter function public.delete_private_device(uuid) set search_path = '';
